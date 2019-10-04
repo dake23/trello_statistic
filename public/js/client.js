c96f90f2eb4d80208f3fa2b9eb05030d754df08f
Alexander Pestov <pestovaw@mail.ru>
2019-10-04 21:59:43 +0300

    Test

--- public/js/client.js
+++ public/js/client.js
@@ -2,6 +2,9 @@
 
 var Promise = TrelloPowerUp.Promise;
 
+
+var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
+var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
 var onBtnClick = function (t, opts) {
