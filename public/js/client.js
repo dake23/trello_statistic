d6c72a202a9d39051972a0e0599e0e651a272d4a
Alexander Pestov <pestovaw@mail.ru>
2019-10-04 21:55:10 +0300

    Test1

--- public/js/client.js
+++ public/js/client.js
@@ -4,21 +4,32 @@ var Promise = TrelloPowerUp.Promise;
 
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
+var onBtnClick = function (t, opts) {
+	console.log('Someone clicked the button');
+  };
+
 TrelloPowerUp.initialize({
-  // Start adding handlers for your capabilities here!
-	// 'card-buttons': function(t, options) {
-	// return t.set("member", "shared", "hello", "world")
-	// .then(function(){
-	// 	  return [{
-	// icon: BLACK_ROCKET_ICON,
-	// 		  text: 'Estimate Size',
-	//       callback: function(t) {
-	//         return t.popup({
-	//           title: "Estimation",
-	//           url: 'estimate.html',
-	//         });
-	//       }
-	// 	  }];
-	// })
-	// },
+	'board-buttons': function (t, opts) {
+		return [{
+		  // we can either provide a button that has a callback function
+		  icon: {
+			dark: WHITE_ICON,
+			light: BLACK_ICON
+		  },
+		  text: 'Callback',
+		  callback: onBtnClick,
+		  condition: 'edit'
+		}, {
+		  // or we can also have a button that is just a simple url
+		  // clicking it will open a new tab at the provided url
+		  icon: {
+			dark: WHITE_ICON,
+			light: BLACK_ICON
+		  },
+		  text: 'URL',
+		  condition: 'always',
+		  url: 'https://trello.com/inspiration',
+		  target: 'Inspiring Boards' // optional target for above url
+		}];
+	  }
 });
